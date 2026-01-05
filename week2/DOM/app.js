79fd1805c75bec8364dfba02ab439fe0c7681ba1
rammahato-dot <rammahato6655@gmail.com>
2026-01-05 22:00:04 +0545

    learnt basics of dom

---
+++ week2/DOM/app.js
@@ -0,0 +1,9 @@
+let links = document.querySelectorAll(".box a");
+
+for (links of links) {
+  links.style.color = "purple";
+}
+
+// for (let i = 0; i < links.length; i++) {
+//   links[i].style.color = "green";
+// }
