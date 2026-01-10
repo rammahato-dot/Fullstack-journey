fbbdfd26edb96daebb5cb95257123c7234242112
rammahato-dot <rammahato6655@gmail.com>
2026-01-10 23:14:23 +0545

    understand about async functions and await keyword and handling rejections with await

--- week2/promises/app.js
+++ week2/promises/app.js
@@ -28,7 +28,7 @@
 //   }
 // );
 
-// //Promises
+//Promises
 
 // function savetoDb(data) {
 //   return new Promise((success, failure) => {
@@ -41,7 +41,7 @@
 //   });
 // }
 
-// // promise chaining
+// promise chaining
 
 // savetoDb("apna college")
 //   .then((result) => {
@@ -63,10 +63,9 @@
 //     console.log("error of promise: ", error);
 //   });
 
+// callback hell
 
-// callback hell 
-
-// h1 = document.querySelector("h1");
+h1 = document.querySelector("h1");
 
 // function changeColor(color, delay, nextColorChange) {
 //   setTimeout(() => {
@@ -85,30 +84,101 @@
 
 // refactoring above code in Promises
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
+
+// function changeColor(color, delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       h1.style.color = color;
+//       resolve("color changed");
+//     }, delay);
+//   });
+// }
+
+// changeColor("red", 1000)
+// .then(()=>{
+//     console.log("red color was completed");
+//     return changeColor("orange", 1000);
+// })
+// .then(()=>{
+//     console.log("orange color was completed");
+//     return changeColor("green", 1000);
+// })
+// .then(()=>{
+//     console.log("green color was completed");
+//     return changeColor("blue", 1000);
+// })
+// .then(()=>{
+//     console.log("blue color was completed");
+// })
+
+// ASYNC KEYWORD
+
+// async function greet() {
+//   throw "404 page not found";
+//   return "hello";
+// }
+
+// greet()
+//   .then(() => {
+//     console.log("promise was resolved");
+//     console.log("result was: ", result);
+//   })
+//   .catch((err) => {
+//     console.log("promise was rejected with err: ", err);
+//   });
+
+// let demo = async () => {
+//   return 5;
+// };
+
+// AWAIT KEYWORD
+
+// function changeColor(color, delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       h1.style.color = color;
+//       console.log(`color chnaged to ${color}!`);
+//       resolve("color changed");
+//     }, delay);
+//   });
+// }
+
+// async function demo() {
+//   await changeColor("red", 1000);
+//   await changeColor("purple", 1000);
+//   await changeColor("green", 1000);
+//   await changeColor("yellow", 1000);
+
+//   console.log();
+// }
+
+// Handling Rejections
 
 function changeColor(color, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      let num = Math.floor(Math.random() * 5) + 1;
+      if (num > 3) {
+        reject("promise rejected");
+      }
       h1.style.color = color;
+      console.log(`color chnaged to ${color}!`);
       resolve("color changed");
     }, delay);
   });
 }
 
-changeColor("red", 1000)
-.then(()=>{
-    console.log("red color was completed");
-    return changeColor("orange", 1000);
-})
-.then(()=>{
-    console.log("orange color was completed");
-    return changeColor("green", 1000);
-})
-.then(()=>{
-    console.log("green color was completed");
-    return changeColor("blue", 1000);
-})
-.then(()=>{
-    console.log("blue color was completed");
-})
+async function demo() {
+  try {
+    await changeColor("red", 1000);
+    await changeColor("purple", 1000);
+    await changeColor("green", 1000);
+    await changeColor("yellow", 1000);
+  } catch (err) {
+    console.log("error caught");
+    console.log(err);
+  }
+
+  console.log();
+}
