9f9dc58a148173e14d315c744ba153cd219cc61d
Ram Mahato <rammahato6655@gmail.com>
2026-01-26 22:59:31 +0545

    added features so that every posts like an unique id and all the pages are connected

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,20 +13,33 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
   {
+    id: uuidv4(),
     username: "rammahato",
-    content: "I love coding!",
+    content: "Coding feels like solving puzzles every day — the more I practice, the more fun it gets!",
   },
   {
+    id: uuidv4(),
     username: "sushil",
-    content: "Hard work is important to achieve success!",
+    content: "Success isn’t just about hard work; it’s about consistency, patience, and learning from failures.",
   },
   {
+    id: uuidv4(),
     username: "rahulkumar",
-    content: "I got selected for my 1st internship!",
+    content: "Thrilled to share that I’ve landed my very first internship — excited to learn and grow!",
+  },
+  {
+    id: uuidv4(),
+    username: "adit",
+    content: "Reading has completely changed how I think — every book feels like a new perspective on life.",
+  },
+  {
+    id: uuidv4(),
+    username: "deepak",
+    content: "Started practicing mindfulness recently, and it’s surprising how much calmer and focused I feel.",
   },
 ];
 
-app.get("/", (req, res) => {
+app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
 });
 
@@ -35,8 +49,16 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
   let { username, content } = req.body;
-  posts.push({ username, content });
-  res.send("post requests working");
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  res.render("show.ejs", { post });
 });
 
 app.listen(port, () => {
