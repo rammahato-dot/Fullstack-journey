e35393b01ac73e2071256922c8ecc051a42e11f8
Ram Mahato <rammahato6655@gmail.com>
2026-01-27 19:33:06 +0545

    added features like edit and delete posts

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -15,27 +17,32 @@ let posts = [
   {
     id: uuidv4(),
     username: "rammahato",
-    content: "Coding feels like solving puzzles every day — the more I practice, the more fun it gets!",
+    content:
+      "Coding feels like solving puzzles every day — the more I practice, the more fun it gets!",
   },
   {
     id: uuidv4(),
     username: "sushil",
-    content: "Success isn’t just about hard work; it’s about consistency, patience, and learning from failures.",
+    content:
+      "Success isn’t just about hard work; it’s about consistency, patience, and learning from failures.",
   },
   {
     id: uuidv4(),
     username: "rahulkumar",
-    content: "Thrilled to share that I’ve landed my very first internship — excited to learn and grow!",
+    content:
+      "Thrilled to share that I’ve landed my very first internship — excited to learn and grow!",
   },
   {
     id: uuidv4(),
     username: "adit",
-    content: "Reading has completely changed how I think — every book feels like a new perspective on life.",
+    content:
+      "Reading has completely changed how I think — every book feels like a new perspective on life.",
   },
   {
     id: uuidv4(),
     username: "deepak",
-    content: "Started practicing mindfulness recently, and it’s surprising how much calmer and focused I feel.",
+    content:
+      "Started practicing mindfulness recently, and it’s surprising how much calmer and focused I feel.",
   },
 ];
 
@@ -57,8 +64,28 @@ app.post("/posts", (req, res) => {
 app.get("/posts/:id", (req, res) => {
   let { id } = req.params;
   let post = posts.find((p) => id === p.id);
-  console.log(post);
   res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  console.log(post);
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
 });
 
 app.listen(port, () => {
